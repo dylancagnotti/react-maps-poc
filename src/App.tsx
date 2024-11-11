c16c772acdc4e99e018433acb51236c50a5c11c6
Dylan Cagnotti <106975297+dylancagnotti@users.noreply.github.com>
2024-11-11 17:48:53 +0100

    chore: implemented the widget

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ function App() {
   return (
     <article style={{ width: "100%" }}>
       <h2> React simple maps</h2>
+      <MapWidget
+        annotations={[
+          {
+            coordinates: [49.56261559320028, 29.99923757515128],
+            name: "Well",
+            onClick: () => alert("Istanbul"),
+          },
+        ]}
+        startPosition={[47, 29]}
+        zoom={6}
+      ></MapWidget>
     </article>
   );
 }
