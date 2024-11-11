98a091e55c293f011eaeb69553da6316829fe157
Dylan Cagnotti <106975297+dylancagnotti@users.noreply.github.com>
2024-11-11 19:20:33 +0100

    implemented card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,68 @@
 import "./App.css";
+import Card from "./components/Card";
 import MapWidget from "./components/MapWidget";
 
 function App() {
   return (
     <article style={{ width: "100%" }}>
       <h2> React simple maps</h2>
-      <MapWidget
-        annotations={[
-          {
-            coordinates: [49.56261559320028, 29.99923757515128],
-            name: "Well",
-            onClick: () => alert("Well"),
-          },
-        ]}
-        startPosition={[47, 29]}
-        zoom={6}
-      ></MapWidget>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+        }}
+      >
+        <Card
+          title="Custom Name"
+          tags={["well-1", "well-2", "well-3"]}
+          description="Wells that I need to see everyday!"
+          mapdata={{
+            annotations: [
+              {
+                coordinates: [49.56261559320028, 29.99923757515128],
+                name: "Well 1",
+                onClick: () => alert("Well 1"),
+              },
+              {
+                coordinates: [49.56261559320028, 29.29923757515128],
+                name: "Well 2",
+                onClick: () => alert("Well 2"),
+              },
+              {
+                coordinates: [49.26261559320028, 29.10923757515128],
+                name: "Well 3",
+                onClick: () => alert("Well 3"),
+              },
+            ],
+            startPosition: [48, 29.5],
+            zoom: 15,
+          }}
+        />
+        <div style={{ width: "500px" }}>
+          <MapWidget
+            annotations={[
+              {
+                coordinates: [49.56261559320028, 29.99923757515128],
+                name: "Well 1",
+                onClick: () => alert("Well 1"),
+              },
+              {
+                coordinates: [49.56261559320028, 29.29923757515128],
+                name: "Well 2",
+                onClick: () => alert("Well 2"),
+              },
+              {
+                coordinates: [49.26261559320028, 29.10923757515128],
+                name: "Well 3",
+                onClick: () => alert("Well 3"),
+              },
+            ]}
+            startPosition={[48, 29.5]}
+            zoom={15}
+            square
+          />
+        </div>
+      </div>
     </article>
   );
 }
